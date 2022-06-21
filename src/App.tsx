2c513b1690f6c9e8b3045f168b3d97ce409ef341
Ankit raj <ankitjaiswal7890@gmail.com>
2022-06-21 17:10:27 +0530

    build: add ffmpeg.wasm

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+const ffmpeg = createFFmpeg({ log: true });
 
 function App() {
-  return (
+  const [ready, setReady] = useState(false);
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  }
+
+  useEffect(() => {
+    load();
+  }, [])
+
+
+  return ready ? (
+    <Fragment>
+
+    </Fragment>
+  ) : (
     <Fragment>
 
     </Fragment>
